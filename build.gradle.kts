55ba10dfa4fd706c65411838d4aea5377d7bc6c0
vlad <vov323215@gmail.com>
2024-07-31 15:53:24 +0300

    accept ToS

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,3 +42,10 @@ tasks {
         dependsOn(":jar:build")
     }
 }
+
+if (hasProperty("buildScan")) {
+    extensions.findByName("buildScan")?.withGroovyBuilder {
+        setProperty("termsOfServiceUrl", "https://gradle.com/terms-of-service")
+        setProperty("termsOfServiceAgree", "yes")
+    }
+}
